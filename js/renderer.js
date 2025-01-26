76ebf3452bf6452b34b2f1f9f63c77b8524afaae
shayshunk <60236335+shayshunk@users.noreply.github.com>
2025-01-25 19:48:50 -1000

    Added comments and made sure sound pauses and plays if clicked twice.

--- js/renderer.js
+++ js/renderer.js
@@ -49,8 +49,7 @@ if (fs.existsSync("UserData.txt")) {
 }
 
 async function addSound(path = null, name = null, vol = null, loopOn = false) {
-  console.log(path);
-
+  // Checking if passing in loaded sound
   if (path === null) {
     soundInformation = await openDialog();
     filePath = soundInformation[0];
@@ -58,6 +57,8 @@ async function addSound(path = null, name = null, vol = null, loopOn = false) {
   else {
     filePath = path;
   }
+
+  // Adding button once path is set
   await addButton(name, vol, loopOn);
 }
 
@@ -144,12 +145,14 @@ async function addButton(name, vol, loopOn) {
   loopButton.addEventListener("click", (e) => setLoop(e));
   container.appendChild(loopButton);
 
+  // Adding rename button
   const pencilButton = document.createElement("button");
   pencilButton.classList.add("pen-button");
   pencilButton.innerHTML =
     '<img src="assets/pencil.png" width="25" height="25">';
   container.appendChild(pencilButton);
 
+  // Adding delete button
   const deleteButton = document.createElement("button");
   deleteButton.classList.add("delete-button");
   deleteButton.innerHTML =
@@ -157,15 +160,18 @@ async function addButton(name, vol, loopOn) {
   deleteButton.addEventListener("click", (e) => deleteFunc(e));
   container.appendChild(deleteButton);
 
+  // Adding volume slider
   const volumeSlider = document.createElement("input");
   volumeSlider.type = "range";
   volumeSlider.min = 0;
   volumeSlider.max = 100;
+  // Checking if loaded value is being passed
   if (vol === null) {
     volumeSlider.value = 100;
   }
   else {
     volumeSlider.value = vol;
+    audio.volume = vol / 100;
   }
   volumeSlider.classList.add("volume-slider");
   volumeSlider.addEventListener("input", (e) => setVolume(e));
@@ -195,56 +201,97 @@ async function addButton(name, vol, loopOn) {
 }
 
 function deleteFunc(event) {
+  // Grabbing button group
   const parentContainer = event.target.parentElement;
   const parentId = parentContainer.id;
 
+  // Remove entire button group
   parentContainer.remove();
 
+  // Making sure sound stops playing if deleted
   sound = buttonList[parentId].audioFile;
   sound.pause();
 
+  // Updating arrays
   buttonList.pop(parentId);
+  userData.pop(parentId);
 
+  // Updating HTML IDs
   for (let i = buttonList.length; i > parentId; i--) {
     soundContainer = document.getElementById(`${i}`);
     soundContainer.id = `${i - 1}`;
   }
+
+  // Updating saved file
+  saveFile();
 }
 
 function setLoop(event) {
+  // Grabbing button group
   const parentId = event.target.parentElement.id;
   const loopButton = buttonList[parentId].loopButton;
 
+  // Checking if button is clicked
   loopButton.className =
     loopButton.className == "loop-clicked" ? "loop-button" : "loop-clicked";
 
+  // Resetting sound if loop is unclicked
   if (loopButton.className == "loop-button") {
     const sound = buttonList[parentId].audioFile;
     sound.pause();
+    sound.currentTime = 0;
   }
+
+  // Updating saved file
+  userData[parentId].loopOn = loopButton.className == "loop-clicked";
+  saveFile();
 }
 
 function setVolume(event) {
+  // Grabbing button group
   const parentId = event.target.parentElement.id;
   const volumeSlider = buttonList[parentId].volumeSlider;
+
+  // Grabbing master and local volume
   const volume = volumeSlider.value / 100;
   const sound = buttonList[parentId].audioFile;
+  const masterVolume = masterVolumeSlider.value / 100;
 
-  sound.volume = volume;
+  // Setting volume
+  sound.volume = volume * masterVolume;
+
+  // Updating saved file
+  userData[parentId].vol = volume * 100;
+  saveFile();
 }
 
 function playSound(event) {
+  // Grabbing button group
   const parentId = event.target.parentElement.id;
   const sound = buttonList[parentId].audioFile;
 
+  // Checking if loop button is toggled
   const isLoop = buttonList[parentId].loopButton.className == "loop-clicked";
 
+  // Sets sound to loop if button is checked
   if (isLoop) {
     sound.loop = true;
+  }
+
+  // Restarts sound if it's already playing
+  if (isSoundPlaying(sound)) {
+    sound.pause();
+    sound.currentTime = 0;
   }
   sound.play();
 }
 
+// Function to check if sound is already playing
+function isSoundPlaying(audio) {
+  return !audio.paused && !audio.muted && audio.currentTime > 0 && audio.readyState >= 2;
+}
+
+// Function to save buttons to file
 function saveFile() {
   const jsonData = JSON.stringify(userData);
   fs.writeFile("UserData.txt", jsonData, function (err) {
